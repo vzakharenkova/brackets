fdbf22078bd75261f722793cdf18251f7620ab56
vzakharenkova <v.s.zakharenkova@gmail.com>
2022-02-18 22:32:46 +0300

    feat: add function brackets check

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let obj = {};
+  bracketsConfig.forEach((el) => {
+    obj[el[0]] = el[1];
+  })
+  let stack = [];
+  for (let i = 0; i < str.length; i++) {
+      let x = str[i];
+      if (x === obj[x]) {
+        if (stack[stack.length - 1] === x) {
+          stack.pop();
+        } else {
+          stack.push(obj[x]);
+        } continue;
+      }
+      if (obj[x]) {
+          stack.push(obj[x]);
+      } else {
+      if (x !== stack.pop()) {
+        return false;
+      }
+    }
+  }
+  return stack.length === 0;
 }
